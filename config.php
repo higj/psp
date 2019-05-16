64f532a8f319f6c4c166e4aaa0a0e3f42a012023
Cthulhu <902786+higj@users.noreply.github.com>
2019-05-16 10:28:18 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 
 define('SIM_OUTPUT_FILE_PATH', './output.txt'); // Simulation output file directory
-define('PARSED_OUTPUT_FILE_PATH', './results/parsed_output'); // Name of the processed Excel file
+define('PARSED_OUTPUT_FILE_PATH', './results/parsed_output'); // Processed output file directory
 
 // If you want to have an Excel table as the output, configure the directory of Composer (for PhpSpreadsheet)
 define('COMPOSER_AUTOLOAD_DIR', 'C:/Users/<USER_NAME>/vendor/autoload.php');
 
 if(file_exists(COMPOSER_AUTOLOAD_DIR)) {
-	require_once COMPOSER_AUTOLOAD_DIR; // Change to your directory
+	require_once COMPOSER_AUTOLOAD_DIR;
 }
